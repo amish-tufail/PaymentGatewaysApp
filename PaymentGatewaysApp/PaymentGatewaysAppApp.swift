be32e2d30bafc30d12c8efd71e9e3387eb632e2e
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2025-06-30 22:23:54 +1000

    Initial Commit

---
+++ PaymentGatewaysApp/PaymentGatewaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentGatewaysAppApp.swift
+//  PaymentGatewaysApp
+//
+//  Created by Amish Tufail on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaymentGatewaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
